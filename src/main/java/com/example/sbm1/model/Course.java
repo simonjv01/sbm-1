3a724d00754d538f1266c5dff6304809386ac687
Simon Vargas <sjvargas01@gmail.com>
2024-08-27 17:23:18 -0400

    created course class

---
+++ src/main/java/com/example/sbm1/model/Course.java
@@ -0,0 +1,30 @@
+package com.example.sbm1.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 100, nullable = false)
+    private String title;
+
+    @Column(name = "subtitle", length = 100, nullable = false)
+    private String subtitle;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+
+}
