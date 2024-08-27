1ff77f3ede85c38871aceb11b504b4db17d8b5ac
Simon Vargas <sjvargas01@gmail.com>
2024-08-27 16:49:09 -0400

    first commit

---
+++ src/main/java/com/example/sbm1/Sbm1Application.java
@@ -0,0 +1,13 @@
+package com.example.sbm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbm1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbm1Application.class, args);
+    }
+
+}
